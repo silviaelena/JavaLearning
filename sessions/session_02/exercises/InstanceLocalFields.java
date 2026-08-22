be25913c8a526ec115b6332bb9b68e2e7e713a8b
silviaelena <silvia.elena110@gmail.com>
2026-08-22 12:23:43 +0300

    Add Session 2 field/local exercise

---
+++ sessions/session_02/exercises/InstanceLocalFields.java
@@ -0,0 +1,15 @@
+package chatgpt_lessons.session2;
+
+/**
+ * @author snistor
+ */
+public class InstanceLocalFields {
+    int number;
+
+    void print() {
+        int local;
+
+        System.out.println(number);
+//        System.out.println(local);
+    }
+}
