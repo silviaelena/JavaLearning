aa50a8e587bd3a0db715dd7f317baaf1bd500dac
silviaelena <silvia.elena110@gmail.com>
2026-08-22 12:24:26 +0300

    Add Session 3 Book test

---
+++ sessions/session_03/exercises/BookTest.java
@@ -0,0 +1,38 @@
+package chatgpt_lessons.session3;
+
+import java.util.Arrays;
+
+/**
+ * @author snistor
+ */
+public class BookTest {
+    public static void main(String[] args) {
+        Book book1 = new Book();
+        Book book2 = new Book();
+
+        book1.title = "Mizerabilii";
+        book1.pages = 1000;
+
+        book2.title = "Falsificatorii de bani";
+        book2.pages = 500;
+
+        Book book3 = book1;
+        book3.addPages(100);
+
+        book3 = book2;
+        book3.title = "Kokoro";
+
+        Book[] library = new Book[3];
+        library[0] = book1;
+        library[1] = book2;
+        library[2] = book1;
+
+        for (Book book : library) {
+            System.out.println("Book: " + book.title + " book pages: " + book.pages);
+        }
+
+        // prediction: 0
+        Book.replace(book1);
+        System.out.println(book1.pages);
+    }
+}
