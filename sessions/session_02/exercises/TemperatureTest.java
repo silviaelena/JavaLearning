5af9adcbbfb78cc68d6f457b386bf460f2bcbb57
silviaelena <silvia.elena110@gmail.com>
2026-08-22 12:24:06 +0300

    Add Session 2 Temperature test

---
+++ sessions/session_02/exercises/TemperatureTest.java
@@ -0,0 +1,22 @@
+package chatgpt_lessons.session2;
+
+/**
+ * @author snistor
+ */
+public class TemperatureTest {
+    public static void main(String[] args) {
+        Temperature temperature1 = new Temperature();
+        Temperature temperature2 = new Temperature();
+
+        temperature1.celsius = 20;
+        temperature2.celsius = -1;
+
+        temperature1.increase(10);
+
+        temperature1.fahrenheit();
+        temperature2.fahrenheit();
+
+        System.out.println(temperature1.isFreezing());
+        System.out.println(temperature2.isFreezing());
+    }
+}
