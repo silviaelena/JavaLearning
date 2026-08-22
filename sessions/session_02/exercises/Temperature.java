3d77325c385b1045efa330c658f89df9ba6724cd
silviaelena <silvia.elena110@gmail.com>
2026-08-22 12:24:01 +0300

    Add Session 2 Temperature exercise

---
+++ sessions/session_02/exercises/Temperature.java
@@ -0,0 +1,20 @@
+package chatgpt_lessons.session2;
+
+/**
+ * @author snistor
+ */
+public class Temperature {
+    double celsius;
+
+    public double fahrenheit() {
+        return (celsius * 1.8) + 32;
+    }
+
+    public boolean isFreezing() {
+        return celsius <= 0;
+    }
+
+    public void increase(double amount) {
+        celsius += amount;
+    }
+}
