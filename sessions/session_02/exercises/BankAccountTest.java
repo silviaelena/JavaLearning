03591002c62427a8d5320899d10e3915e3fed932
silviaelena <silvia.elena110@gmail.com>
2026-08-22 12:23:39 +0300

    Add Session 2 BankAccount test

---
+++ sessions/session_02/exercises/BankAccountTest.java
@@ -0,0 +1,25 @@
+package chatgpt_lessons.session2;
+
+/**
+ * @author snistor
+ */
+public class BankAccountTest {
+    public  static void main(String[] args) {
+        BankAccount bankAccountAna = new BankAccount("Ana", 50000);
+        BankAccount bankAccountBob = new BankAccount("Bob", 20000);
+        // wrong
+//        BankAccount bankAccountCa;
+//        bankAccountCa.printBalance();
+
+        bankAccountAna.deposit(10000);
+        bankAccountBob.deposit(20000);
+        bankAccountAna.withdraw(5000);
+        bankAccountBob.withdraw(7000);
+        bankAccountAna.withdraw(5000);
+        bankAccountBob.deposit(7000);
+        bankAccountBob.withdraw(70000);
+
+        bankAccountAna.printBalance();
+        bankAccountBob.printBalance();
+    }
+}
